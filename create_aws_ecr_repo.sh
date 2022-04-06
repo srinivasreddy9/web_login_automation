a43e5dcc0a335037396a9a7a7c9f8e851417e7b3
srinivasreddy9 <75516603+srinivasreddy9@users.noreply.github.com>
2022-04-06 16:00:30 +0530

    Update create_aws_ecr_repo.sh

--- create_aws_ecr_repo.sh
+++ create_aws_ecr_repo.sh
@@ -1,9 +1,9 @@
 # RUN me where kubectl is available,& make sure to replace account,region etc
 #
-ACCOUNT=403959134869
+ACCOUNT=363445897050
 REGION=ap-south-1
 SECRET_NAME=${REGION}-ecr-registry
-EMAIL=ybmadhu404@gmail.com
+EMAIL=srinivasaws05@gmail.com
 
 #
 # Fetch token (which will expire in 12 hours)
